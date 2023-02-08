8c96b7a3347bd5aaeed08f1c846915d0740eb8f2
Avinash choubey <104202659+Avinash-choubey-4719@users.noreply.github.com>
2023-02-08 23:47:45 +0530

    Add files via upload

--- leftFactoring.cpp
+++ leftFactoring.cpp
@@ -1,85 +1,127 @@
 #include <bits/stdc++.h>
-#include <iostream>
 using namespace std;
+int main()
+{
+    long long int i,j,k,l,n,m=9999999999,mini,ma=0;
+    string s[100],st,ch,sc="",result,fs,maxi,rs="";
+    vector<string>ss;
+    vector<string>sp;
+    cin>>n;
 
-int main() {
-  map<char, vector<string>> prod;
-  int n;
-  cout << "Enter number of productions : ";
-  cin >> n;
-  cout << "Enter productions : " << endl;
-  for (int i = 0; i < n; i++) {
-    char ch;
-    string s;
-    cin >> ch;
-    cout << " -> ";
-    cin >> s;
-    prod[ch].push_back(s);
-  }
-
-  cout << "Given grammar is :" << endl << endl;
-  for (map<char, vector<string>>::iterator i = prod.begin(); i != prod.end();
-       i++) {
-    cout << i->first << " -> ";
-    for (int j = 0; j < i->second.size(); j++) {
-      cout << i->second[j];
-      if (j != i->second.size() - 1)
-        cout << " | ";
+    for(i=1;i<=n;i++)
+    {
+        cin>>s[i];
     }
-    cout << endl;
-  }
+    for(i=1;i<=n;i++)
+    {
+        st=s[i];
+        sc="";
 
-  for (map<char, vector<string>>::iterator itr = prod.begin();
-       itr != prod.end(); ++itr) {
-
-    vector<string> v = itr->second;
-    int min_len = INT_MAX;
-    for (int j = 0; j < v.size(); j++) {
-      if (v[j].length() < min_len)
-        min_len = v[j].length();
-    }
-    for (int i = 0; i < v.size(); i++) {
-      int prev = 0, curr = 0, len = 0;
-
-      for (int j = 1; j <= min_len; j++) {
-        string s = v[i].substr(0, j);
-        curr = 0;
-        for (int k = 0; k < v.size(); k++) {
-          if (k == i)
-            continue;
-          if (v[k].substr(0, j).compare(s) == 0)
-            curr++;
+        for(j=0;j<st.length();j++)
+        {
+            if(i==1)
+            {
+                fs=st[0];
+            }
+            if(st[j]=='=')
+            {
+               l=j;
+            }
         }
 
-        if (curr < prev)
-          break;
-        prev = curr;
-        len = j;
-      }
-      if (prev != 0) {
+        if(i==1)
+        {
 
-        char ch = 'A' + prod.size();
-        string s = v[i].substr(0, len);
+        for(k=l+1;k<st.length();k++)
+        {
+            if(st[k]=='|')
+            {
+                ss.push_back(sc);
+                sc="";
+            }
+            if(st[k]!='|')
+            {
+                ch=st[k];
+                sc=sc+ch;
+            }
 
-        for (int j = 0; j < v.size(); j++) {
-          if (v[j].substr(0, len).compare(s) == 0) {
-            v[j] = s + ch;
-            itr->second[j] = v[j];
-            prod[ch].push_back(v[j].substr(len, v[j].size() - len));
-          }
         }
-      }
+        ss.push_back(sc);
+        }
+        //cout<<sc<<endl;
+
+
     }
-  }
-  cout << "Grammar after removing left factoring is :" << endl << endl;
-  for (map<char, vector<string>>::iterator i = prod.begin(); i != prod.end();
-       i++) {
-    cout << i->first << " -> ";
-    for (int j = 0; j < i->second.size(); j++) {
-      cout << i->second[j];
-      if (j != i->second.size() - 1)
-        cout << " | ";
+
+    for(k=0;k<ss.size();k++)
+    {
+        mini=ss[k].size();
+        m=min(m,mini);
+        maxi=ss[k];
+        //cout<<ss[k]<<endl;
+
     }
-    cout << endl;
-  }
+    //cout<<maxi<<endl;
+
+
+
+
+    for(k=0;k<m;k++)
+    {
+        //cout<<ss[0][k]<<endl;
+    }
+
+    for (int i=0; i<m; i++)
+    {
+
+        char current = ss[0][i];
+
+        for (int j=1 ; j<ss.size(); j++)
+        {
+            if (ss[j][i] != current)
+            {
+                   break;
+            }
+             result.push_back(current);
+        }
+
+    }
+
+    for(j=0;j<ss.size();j++)
+    {
+        maxi=ss[j];
+        //cout<<maxi<<result.length()<<endl;
+    for(k=0;k<maxi.length();k++)
+    {
+
+
+        if(k>=result.length())
+        {
+            rs=rs+maxi[k];
+
+
+        }
+
+        //cout<<rs<<endl;
+
+    }
+            if(j!=ss.size()-1)
+            {
+                  rs=rs+'|';
+            }
+    }
+
+    cout<<fs<<"="<<result<<fs<<"'"<<endl;
+    cout<<fs<<"'"<<"="<<rs<<endl;
+
+    for(i=2;i<=n;i++)
+    {
+        cout<<s[i]<<endl;
+    }
+
+
+
+    return 0;
+
+
 }
